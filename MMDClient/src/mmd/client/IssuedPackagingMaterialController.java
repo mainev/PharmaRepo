7228d1ba7e1034b7aa809972b42544563afe37d0
maine <mainevillarias@gmail.com>
2015-05-12 10:55:31 +0800

    vrNo and shelfLife for poduct entity

--- MMDClient/src/mmd/client/IssuedPackagingMaterialController.java
+++ MMDClient/src/mmd/client/IssuedPackagingMaterialController.java
@@ -5,12 +5,20 @@
  */
 package mmd.client;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import mmd.client.entity.IssuedPackagingMaterial;
 
 /**
@@ -18,7 +26,7 @@ import mmd.client.entity.IssuedPackagingMaterial;
  * @author maine
  */
 public class IssuedPackagingMaterialController implements Initializable {
-    
+
     //*************************TABLE******************************************/
     @FXML
     TableView<IssuedPackagingMaterial> _tableViewIssuedPackagingMaterial;
@@ -36,13 +44,27 @@ public class IssuedPackagingMaterialController implements Initializable {
     TableColumn<IssuedPackagingMaterial, String> _colUnit;
     @FXML
     TableColumn<IssuedPackagingMaterial, String> _colIssuedBy;
-    
+
     //************************************************************************//
     @Override
-    public void initialize(URL url, ResourceBundle rb){
+    public void initialize(URL url, ResourceBundle rb) {
     }
-    
+
     @FXML
-    private void handleNewButton(){
+    private void handleNewButton() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/_transactPackagingMaterialForm.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setTitle("Packaging Material Transaction");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("EXCEPTIONS: \n" + e.getMessage());
+        }
+        
+        
     }
 }
